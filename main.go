dbab45ed99b3cab29f1dd0813a753856e15bc634
yudhie123 <yudhie123@gmail.com>
2022-02-20 20:38:29 +0700

    feat: add connect database

--- main.go
+++ main.go
@@ -1,64 +1,165 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 	"net/http"
+	"time"
 )
 
-// Album represents data about a record album
-type album struct {
-	ID     string  `json:"id"`
+type Album struct {
+	ID     int64   `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+	Price  float32 `json:"price"`
 }
 
-// album slice to seed record album data
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+type CreateAlbum struct {
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float32 `json:"price"`
 }
 
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
-
-func postAlbum(c *gin.Context) {
-	var newAlbum album
-
-	// Call BindJSON to bind the received JSON to
-	// newAlbum
-
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-
-	// Add new Album to the slice
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, albums)
-}
-
-func getAlbumById(c *gin.Context) {
-	id := c.Param("id")
-
-	// Loop over the list of albums,looking for
-	// an album whose ID value matches the parameter
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
-}
+var db *sql.DB
 
 func main() {
+	// Capture connection properties
+	//cfg := mysql.Config{
+	//	User: os.Getenv("DBUSER"),
+	//	//Passwd: os.Getenv("DBPASS"),
+	//	Net:    "tcp",
+	//	Addr:   "127.0.0.1:3306",
+	//	DBName: "recordings",
+	//}
+	// Get a database handle
+	// "root:password1@tcp(127.0.0.1:3306)/test"
+	var err error
+	//db, err = sql.Open("mysql", cfg.FormatDSN())
+	db, err = sql.Open("mysql", "root@tcp(127.0.0.1:3306)/recordings")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// See "Important settings" section.
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	fmt.Println("Connected!")
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/artist/:name", albumsByArtist)
 	router.GET("/albums/:id", getAlbumById)
 	router.POST("/albums", postAlbum)
 
 	router.Run("localhost:8080")
+}
+
+// albumsByArtist queries for albums that have the specified artist name.
+func albumsByArtist(c *gin.Context) {
+	name := c.Param("name")
+	// An albums slice to hold data from returned row
+	var albums []Album
+
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"message": "albumsByArtist not found"})
+		return
+	}
+
+	defer rows.Close()
+	// Loop through rows, using Scan to assign column data to struct fields
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"message": "albumsByArtist not found"})
+			return
+		}
+		albums = append(albums, alb)
+	}
+
+	if err := rows.Err(); err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"message": "albumsByArtist not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, albums)
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	var albums []Album
+
+	rows, err := db.Query("SELECT * FROM album")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Not Found"})
+			return
+		}
+		albums = append(albums, alb)
+	}
+
+	if err := rows.Err(); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Not Found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, albums)
+}
+
+func postAlbum(c *gin.Context) {
+	var newAlbum CreateAlbum
+
+	// Call BindJSON to bind the received JSON to newAlbum
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Insert data to database
+	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)",
+		newAlbum.Title, newAlbum.Artist, newAlbum.Price)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Error insert"})
+		return
+	}
+
+	// Get the new album's generated ID for the client.
+	id, err := result.LastInsertId()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Error generated last id"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": id})
+}
+
+func getAlbumById(c *gin.Context) {
+	var album Album
+	id := c.Param("id")
+
+	// Loop over the list of albums,looking for
+	// an album whose ID value matches the parameter
+	err := db.QueryRow("SELECT * FROM album WHERE id = ?", id).Scan(&album.ID, &album.Title, &album.Artist, &album.Price)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "No result"})
+		return
+	}
+
+	c.JSON(http.StatusOK, album)
 }
